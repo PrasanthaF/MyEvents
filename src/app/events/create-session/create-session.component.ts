2ac0ba35245b0a439cbcac1f692630979f8d3075
Prasantha Fernando <fprasantha@gmail.com>
2017-09-02 18:05:16 -0400

    Adding Reactive form for create session

--- src/app/events/create-session/create-session.component.ts
+++ src/app/events/create-session/create-session.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ISession } from "../../shared/event.model";
 
 @Component({
   selector: 'todo-create-session',
@@ -6,10 +8,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-session.component.css']
 })
 export class CreateSessionComponent implements OnInit {
+  newSessionForm: FormGroup;
+  name: FormControl;
+  presenter: FormControl;
+  duration: FormControl;
+  level: FormControl;
+  abstract: FormControl;
 
   constructor() { }
 
   ngOnInit() {
+    this.name = new FormControl('', Validators.required)
+    this.presenter = new FormControl('', Validators.required)
+    this.duration = new FormControl('', Validators.required)
+    this.level = new FormControl('', Validators.required)
+    this.abstract = new FormControl('', [Validators.required,
+    Validators.maxLength(400)])
+
+    this.newSessionForm = new FormGroup({
+      name: this.name,
+      presenter: this.presenter,
+      duration: this.duration,
+      level: this.level,
+      abstract: this.abstract
+    })
+  }
+
+  saveSession(formValues){
+    let session:ISession = {
+      id: undefined,
+      name: formValues.name,
+      presenter: formValues,
+      duration: +formValues,
+      level: formValues,
+      abstract: formValues,
+      voters: []
+    }
+    console.log(formValues)
   }
 
 }
